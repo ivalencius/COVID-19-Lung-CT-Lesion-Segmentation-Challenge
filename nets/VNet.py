5e1b51df624b0cf9a4c1e24fc315a4cf31643ebd
ivalencius <ilanvalencius@gmail.com>
2022-04-23 23:03:33 -0400

    new nets

---
+++ nets/VNet.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import monai
+import torch
+from torchsummary import summary
+from torchviz import make_dot
+
+
+def VNet(num_classes):
+    net = monai.networks.nets.HighResNet(
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=num_classes,
+        dropout_prob=0.1,
+    )
+    return net
+
+if __name__ == '__main__':
+    device = 'cpu'
+    model = VNet(2)
+    summary(model, (1, 192, 192, 32), device=device)
+    x = torch.rand((1, 1, 192, 192, 32), device=device)
+    out = model(x)
+    graph = make_dot(out)
+    graph.render('VNet', view=True)
+    
