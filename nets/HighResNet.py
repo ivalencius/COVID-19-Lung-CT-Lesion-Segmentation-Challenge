9cbc2915dc6ade6dd8e8924d265f2c16855202a8
colegvozdas1 <74271253+colegvozdas1@users.noreply.github.com>
2022-05-10 18:11:20 -0400

    Update HighResNet.py
    added parameter updatability from monai site, can now change structure of net

--- nets/HighResNet.py
+++ nets/HighResNet.py
@@ -4,6 +4,20 @@ import torch
 from torchsummary import summary
 from torchviz import make_dot
 
+#edited from Monai networks HRNet site at
+#https://docs.monai.io/en/stable/_modules/monai/networks/nets/highresnet.html
+
+DEFAULT_LAYER_PARAMS_3D = (
+    # initial conv layer
+    {"name": "conv_0", "n_features": 16, "kernel_size": 3},
+    # residual blocks
+    {"name": "res_1", "n_features": 16, "kernels": (3, 3), "repeat": 3},
+    {"name": "res_2", "n_features": 32, "kernels": (3, 3), "repeat": 3},
+    {"name": "res_3", "n_features": 64, "kernels": (3, 3), "repeat": 3},
+    # final conv layers
+    {"name": "conv_1", "n_features": 80, "kernel_size": 1},
+    {"name": "conv_2", "kernel_size": 1},
+)
 
 def HighResNet(num_classes):
     net = monai.networks.nets.HighResNet(
@@ -12,6 +26,8 @@ def HighResNet(num_classes):
         out_channels=num_classes,
         dropout_prob=0.1,
         acti_type=('leakyrelu', {'inplace': True}),
+        layer_params: Sequence[Dict] = DEFAULT_LAYER_PARAMS_3D,
+        
     )
     return net
 
