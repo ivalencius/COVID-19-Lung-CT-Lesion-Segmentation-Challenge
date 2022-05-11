565bba0c5358ba88aa0f08e91d406bf5c3fd5b2a
colegvozdas1 <74271253+colegvozdas1@users.noreply.github.com>
2022-05-11 14:38:46 -0400

    Update HighResNet.py

--- nets/HighResNet.py
+++ nets/HighResNet.py
@@ -11,9 +11,9 @@ DEFAULT_LAYER_PARAMS_3D = (
     # initial conv layer
     {"name": "conv_0", "n_features": 16, "kernel_size": 3},
     # residual blocks
-    {"name": "res_1", "n_features": 16, "kernels": (3, 3), "repeat": 2},
-    {"name": "res_2", "n_features": 32, "kernels": (3, 3), "repeat": 2},
-    {"name": "res_3", "n_features": 64, "kernels": (3, 3), "repeat": 2},
+    {"name": "res_1", "n_features": 16, "kernels": (3, 3), "repeat": 1},
+    {"name": "res_2", "n_features": 32, "kernels": (3, 3), "repeat": 1},
+    {"name": "res_3", "n_features": 64, "kernels": (3, 3), "repeat": 1},
     # final conv layers
     {"name": "conv_1", "n_features": 80, "kernel_size": 1},
     {"name": "conv_2", "kernel_size": 1},
