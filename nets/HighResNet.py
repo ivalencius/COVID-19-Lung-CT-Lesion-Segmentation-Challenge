32586d6a28d51d6dba773bb0d50d04f7dd764df3
ivalencius <ilanvalencius@gmail.com>
2022-05-17 17:00:05 -0400

    explicit layers

--- nets/HighResNet.py
+++ nets/HighResNet.py
@@ -4,14 +4,30 @@ import torch
 from torchsummary import summary
 from torchviz import make_dot
 
+#edited from Monai networks HRNet site at
+#https://docs.monai.io/en/stable/_modules/monai/networks/nets/highresnet.html
+
+DEFAULT_LAYER_PARAMS_3D = (
+    # initial conv layer
+    {"name": "conv_0", "n_features": 16, "kernel_size": 3},
+    # residual blocks
+    {"name": "res_1", "n_features": 16, "kernels": (3, 3), "repeat": 3},
+    {"name": "res_2", "n_features": 32, "kernels": (3, 3), "repeat": 3},
+    {"name": "res_3", "n_features": 64, "kernels": (3, 3), "repeat": 3},
+    # final conv layers
+    {"name": "conv_1", "n_features": 80, "kernel_size": 1},
+    {"name": "conv_2", "kernel_size": 1},
+)
 
 def HighResNet(num_classes):
     net = monai.networks.nets.HighResNet(
         spatial_dims=3,
         in_channels=1,
         out_channels=num_classes,
-        dropout_prob=0.1,
-        acti_type=('leakyrelu', {'inplace': True}),
+        dropout_prob=0.5,
+        acti_type=('relu6', {'inplace': True}),
+        layer_params= DEFAULT_LAYER_PARAMS_3D,
+        
     )
     return net
 
